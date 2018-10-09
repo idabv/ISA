86ac8616d82b5781cf1e520a11f42689afc7e877
Aimas Lund <33122600+aimas-lund@users.noreply.github.com>
2018-10-09 12:29:13 +0200

    Revert "some instructions"
    This reverts commit 5b277a4bb144e9fdb6a08a2a6722d6fb759a5c65.

--- isa-sim.py
+++ isa-sim.py
@@ -306,16 +306,7 @@ def decode(instruction):
     com = instruction[0]
     if com = 'ADD':
         instruction[1] = instruction[2] + instruction[3]
-    elif com = 'SUB':
-        instruction[1] = instruction[2] - instruction[3]
-    elif com = 'OR':
-        instruction[1] = instruction[2] or instruction[3]
-    elif com = 'AND':
-        instruction[1] = instruction[2] and instruction[3]
-    elif com = 'NOT':
-        instruction[1] = not(instruction[2])
-
-
+    elif com =
 current_cycle = 0
 program_counter = 0
 
